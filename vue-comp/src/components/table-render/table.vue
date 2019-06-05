cb368c880d51e27fd34694f6f0286992caaed3bf
yingjh <yjh.1234@qq.com>
2019-06-05 18:47:52 +0800

    function render

---
+++ vue-comp/src/components/table-render/table.vue
@@ -0,0 +1,54 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="col in columns" :key="col.key">{{col.title}}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(row, index) in data" :key="index">
+        <td v-for="col in columns" :key="index + col.key">{{row[col.key]}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    columns: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    data: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style>
+table {
+  width: 100%;
+  border-spacing: 0;
+  border-collapse: collapse;
+  empty-cells: show;
+  border: 1px solid #e9e9e9;
+}
+table th {
+  background: #f7f7f7;
+  color: #5c6b77;
+  font-weight: 600;
+  white-space: nowrap;
+}
+table th, table td {
+  padding: 8px 16px;
+  border: 1px solid #e9e9e9;
+  text-align: left;
+}
+</style>
